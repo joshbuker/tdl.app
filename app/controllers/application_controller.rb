4068ce8bd5b413f02c4d8bd310ca730ec61a35cc
Joshua Buker <buker.joshua@gmail.com>
2019-10-24 15:17:44 -0700

    Add Hello World

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Typically you don't have routable methods in ApplicationController, as it's
+  # intended more as an abstract controller to inherit from. With this however,
+  # we're delegating all rendering to Vue as a single page application.
+  def index
+    render template: 'application/index'
+  end
 end
