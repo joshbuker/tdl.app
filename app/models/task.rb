85b93785b6fe2610ed14942280ef6701f779f094
Joshua Buker <buker.joshua@gmail.com>
2020-05-14 00:19:33 -0700

    Show all tags even when searching by tag

--- app/models/task.rb
+++ app/models/task.rb
@@ -183,7 +183,7 @@ class Task < ApplicationRecord
       title: title,
       completed: completed,
       list_title: list.title,
-      tags: tags.map{ |tag| { title: tag.title, color: tag.color, text_color: tag.text_color } }
+      tags: tags.reload.map{ |tag| { title: tag.title, color: tag.color, text_color: tag.text_color } }
     }
   end
 
