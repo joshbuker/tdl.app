c92e6ce6e44adbef792e5bf3adf8d4aef04698c0
Kurt Apple <>
2020-06-27 23:34:00 -0700

    callback and validations for redundant, circular, redundant, OR circular rules.

--- spec/models/rule_spec.rb
+++ spec/models/rule_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Rule do
   subject { build :rule }
 
-  it 'has valid factory.' do
+  it 'has valid factory' do
     expect(subject).to be_valid
   end
 
@@ -15,5 +15,52 @@ RSpec.describe Rule do
   context 'validations' do
     it { is_expected.to validate_uniqueness_of(:pre).scoped_to(:post_id) }
     it { is_expected.to validate_uniqueness_of(:post).scoped_to(:pre_id) }
+
+    describe 'prune_redundant_rules' do
+      it 'prunes redundant rules involving pre' do
+        # A -> B
+        # A -> C
+        # B -> C (removes A -> C)
+        a = create :task, title: 'a'
+        b = create :task, title: 'b'
+        c = create :task, title: 'c'
+        ab = create :rule, pre: a, post: b
+        ac = create :rule, pre: a, post; c
+
+        expect(ac).to be_present
+
+        bc = create :rule, pre: b, post: c
+        expect(ac).not_to be_present
+      end
+    end
+
+    describe 'circular_rule' do
+      it 'prevents cyclic rule from being created' do
+        # validation
+        # A -> B -> C -> D
+        # D -> A
+        a = create :task, title: 'a'
+        b = create :task, title: 'b'
+        c = create :task, title: 'c'
+        d = create :task, title: 'd'
+        ab = create :rule, pre: a, post: b
+        bc = create :rule, pre: b, post; c
+        cd = create :rule, pre: c, post: d
+        expect{create :rule, pre: d, post: a}.to raise_error
+      end
+    end
+
+    describe 'redundant_rule' do
+      if 'prevents redundant rules from being created' do
+        a = create :task, title: 'a'
+        b = create :task, title: 'b'
+        c = create :task, title: 'c'
+        d = create :task, title: 'd'
+        ab = create :rule, pre: a, post: b
+        bc = create :rule, pre: b, post; c
+        cd = create :rule, pre: c, post: d
+        expect{create :rule, pre: a, post: d}.to raise_error
+      end
+    end
   end
 end
