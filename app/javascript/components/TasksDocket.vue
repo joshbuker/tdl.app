9610dd7c0610f1864d25814f10073b6d1ec51ace
Josh Buker <crypto@joshbuker.com>
2020-08-15 20:44:35 -0700

    Update tasks to be draggable
    Unfortunately does not persist yet, haven't figured out the best way to
    implement a sortable value that works across multiple tags/lists and doesn't run
    into issues.

--- app/javascript/components/TasksDocket.vue
+++ app/javascript/components/TasksDocket.vue
@@ -3,17 +3,19 @@
     <h3 class="text-center pt-2">{{ title }}</h3>
     <hr class="mb-0">
     <ul class="list-group overflow-auto" style="max-height: 75vh">
-      <li v-for="(task, index) in tasks" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
-          :task='task' :index='index' :key='task.title'
-          data-toggle='modal' data-target='#taskDetailModal'
-          @click="showDetailModal(task)">
-        {{ task.title }}
-        <div><!-- Group tags together visually -->
-          <span v-for='tag in task.tags' class="badge badge-secondary badge-pill ml-1 border border-dark" v-bind:style="{ 'background-color': tag.color, 'color': tag.text_color }">
-            &nbsp;
-          </span>
-        </div>
-      </li>
+      <draggable :list="tasks" group="tasksdocket" @change="updateTaskOrder">
+        <li v-for="(task, index) in tasks" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+            :task='task' :index='index' :key='task.title'
+            data-toggle='modal' data-target='#taskDetailModal'
+            @click="showDetailModal(task)">
+          {{ task.title }}
+          <div><!-- Group tags together visually -->
+            <span v-for='tag in task.tags' class="badge badge-secondary badge-pill ml-1 border border-dark" v-bind:style="{ 'background-color': tag.color, 'color': tag.text_color }">
+              &nbsp;
+            </span>
+          </div>
+        </li>
+      </draggable>
     </ul>
     <p v-if="tasks.length == 0" class="text-center text-muted mt-3">
       <strong>Nothing yet!</strong>
@@ -30,9 +32,14 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
   props: {
-    tasks: Array,
+    tasks: {
+      required: true,
+      type: Array
+    },
     title: String
   },
   data: function() {
@@ -49,7 +56,13 @@ export default {
     },
     showDetailModal(task) {
       this.$emit('clicked-show-detail', task);
+    },
+    updateTaskOrder(tasks) {
+      console.log(tasks);
     }
+  },
+  components: {
+    draggable
   }
 };
 </script>
