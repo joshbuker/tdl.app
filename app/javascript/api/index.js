4776145569f522a57ab5f3a83eb0a70de8e33ed1
Josh Buker <crypto@joshbuker.com>
2020-10-05 15:00:47 -0700

    Separate api into logical chunks

---
+++ app/javascript/api/index.js
@@ -0,0 +1,11 @@
+import lists from './modules/lists.js.erb'
+import logout from './modules/logout.js.erb'
+import tags from './modules/tags.js.erb'
+import tasks from './modules/tasks.js.erb'
+
+export default {
+  ...lists,
+  ...logout,
+  ...tags,
+  ...tasks
+}
