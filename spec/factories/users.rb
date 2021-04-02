25837c8a69bb1d404d5b4f505dbc546850a909b6
Josh Buker <crypto@joshbuker.com>
2021-04-01 21:45:49 -0700

    Update user factory to include randomized password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     given_name { Faker::Name.first_name }
     family_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end
