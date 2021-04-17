df1868057b5d1bfb5bb8c621ac222258020af043
Josh Buker <crypto@joshbuker.com>
2021-04-17 12:50:35 -0700

    Generate RSpec install

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,11 @@ module ApiTdlApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configure rails generators
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+    end
   end
 end
