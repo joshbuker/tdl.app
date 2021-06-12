7b0e3be2d62a29890d97e15181c81b0ef4acae3b
Josh Buker <crypto@joshbuker.com>
2021-06-12 10:03:38 -0700

    Add loading bar for API calls

--- quasar.conf.js
+++ quasar.conf.js
@@ -89,7 +89,13 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      config: {},
+      config: {
+        loadingBar: {
+          color: 'blue',
+          position: 'bottom',
+          size: '5px',
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -104,7 +110,8 @@ module.exports = configure(function (ctx) {
       // Quasar plugins
       plugins: [
         'LocalStorage',
-        'Notify'
+        'Notify',
+        'LoadingBar',
       ]
     },
 
