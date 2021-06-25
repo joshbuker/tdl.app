6978ffe352766d58575fb20c6f61a945b2125d6b
Kurt Apple <kapplepi@gmail.com>
2021-06-24 18:50:16 -0700

    adding seeds take ii

---
+++ db/seeds/user.rb
@@ -0,0 +1,10 @@
+testuser = User.create!(
+  username: 'awesome',
+  given_name: 'awesome',
+  family_name: 'username',
+  email: 'username@awesome',
+  password: 'Awesome1!',
+  terms_and_conditions: DateTime.new(1970,1,2,1)
+)
+
+
