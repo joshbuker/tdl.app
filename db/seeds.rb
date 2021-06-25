6f283d81a039adeb91596bfec75b9651666d339e
Kurt Apple <kapplepi@gmail.com>
2021-06-24 18:49:40 -0700

    add test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ApplicationRecord.transaction do
+  seed_file = Rails.root.join('db', 'seeds', "#{Rails.env.downcase}.rb")
+  puts "Loading seed file from: #{seed_file}"
+  load(seed_file)
+end
