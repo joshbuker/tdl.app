d92b7fd328f37c7f84087a4a564d3e06a4a55319
Josh Buker <crypto@joshbuker.com>
2021-07-08 01:14:40 -0700

    Add dragging to lists control

--- src/components/ListsControl.vue
+++ src/components/ListsControl.vue
@@ -1,35 +1,46 @@
 <template>
   <q-list>
-    <q-item clickable v-ripple v-if="!editMode">
-      <q-item-section>
-        <q-item-label>All Tasks</q-item-label>
-        <q-item-label caption>{{ allTasksCount }} tasks</q-item-label>
-      </q-item-section>
-    </q-item>
-    <q-item
-      clickable
-      v-ripple
-      v-for="list in lists"
-      :key="list.id"
-      :value="list.title"
+    <draggable
+      v-model="lists"
+      :disabled="!editMode"
+      item-key="id"
+      handle=".handle"
+      @start="dragging = true"
+      @end="dragging = false"
     >
-      <q-item-section avatar v-show="editMode">
-        <q-btn flat round icon="fas fa-bars" />
-      </q-item-section>
+      <template #header>
+        <q-item clickable v-ripple v-if="!editMode">
+          <q-item-section>
+            <q-item-label>All Tasks</q-item-label>
+            <q-item-label caption>{{ allTasksCount }} tasks</q-item-label>
+          </q-item-section>
+        </q-item>
+      </template>
 
-      <q-item-section>
-        <q-item-label>{{ list.title }}</q-item-label>
-        <q-item-label caption>0 tasks</q-item-label>
-      </q-item-section>
+      <template #item="{ element }">
+        <q-item
+          v-bind:clickable="!editMode"
+          v-bind:v-ripple="!editMode"
+        >
+          <q-item-section avatar v-show="editMode">
+            <q-btn flat round icon="fas fa-bars" class="handle" />
+          </q-item-section>
 
-      <q-item-section avatar v-if="editMode">
-        <q-btn flat round color="green" icon="fas fa-pencil-alt" />
-      </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ element.title }}</q-item-label>
+            <q-item-label caption>0 tasks</q-item-label>
+          </q-item-section>
 
-      <q-item-section avatar v-if="editMode">
-        <q-btn flat round color="red" icon="fas fa-trash" />
-      </q-item-section>
-    </q-item>
+          <q-item-section avatar v-if="editMode">
+            <q-btn flat round color="green" icon="fas fa-pencil-alt" />
+          </q-item-section>
+
+          <q-item-section avatar v-if="editMode">
+            <q-btn flat round color="red" icon="fas fa-trash" />
+          </q-item-section>
+        </q-item>
+      </template>
+    </draggable>
   </q-list>
 
   <q-input outlined class="q-ma-md" label="Create new list" v-if="editMode">
@@ -52,14 +63,17 @@
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue'
 import { useStore } from '../store'
+import draggable from 'vuedraggable'
 
 export default defineComponent({
   name: 'ListsControl',
+  components: { draggable },
 
   setup() {
     const $store = useStore()
     const allTasksCount = ref(0)
     const editMode = ref(false)
+    const dragging = ref(false)
 
     const lists = computed({
       get: () => $store.state.lists.lists,
@@ -71,6 +85,7 @@ export default defineComponent({
     return {
       allTasksCount,
       editMode,
+      dragging,
       lists
     }
   }
