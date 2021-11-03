e40a4e9cf71757764360e9a107938809853c0887
Josh Buker <crypto@joshbuker.com>
2021-11-03 15:31:54 -0700

    Using a string seems to fix the heroku issue, no clue why yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
   rescue_from ActiveRecord::RecordInvalid, with: :not_processable
-  # FIXME: This breaks on heroku for some fucking reason, idk
   rescue_from "ActiveRecord::DeleteRestrictionError", with: :not_processable
   rescue_from NotImplementedError, with: :endpoint_not_implemented
 
