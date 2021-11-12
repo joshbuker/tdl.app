64af50cca1c95977daefbb82c819e85c0adc6415
Kurt Apple <kapplepi@gmail.com>
2021-11-11 20:29:19 -0800

    fixing logic once and for all in rules

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -33,7 +33,7 @@ class Rule < ApplicationRecord
 
     return unless pre.present? && post.present?
     return unless pre.persisted? && post.persisted?
-    return unless post.all_posts.include?(pre) && pre.all_pres.include?(post)
+    return unless post.all_posts.include?(pre) || pre.all_pres.include?(post)
 
     # pre is already a post of post
     errors.add(
@@ -53,7 +53,7 @@ class Rule < ApplicationRecord
 
     return unless pre.present? && post.present?
     return unless pre.persisted? && post.persisted?
-    return unless post.all_pres.include?(pre) && pre.all_posts.include?(post)
+    return unless post.all_pres.include?(pre) || pre.all_posts.include?(post)
 
     errors.add(
       :base,
